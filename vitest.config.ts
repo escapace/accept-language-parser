ca39ebf400399b64433a7636ebc878abf93b6aa8
yyxi <178086254+yyxi@users.noreply.github.com>
2024-05-05 16:57:26 +0000

    fix: updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
+      exclude: ['**/example*.ts'],
+      include: ['src/**'],
       provider: 'v8',
     },
+    passWithNoTests: true,
   },
 })
