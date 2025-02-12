588289d65ab294b979fbc836fe4c6cc1aff00608
yyxi <178086254+yyxi@users.noreply.github.com>
2025-02-12 12:24:48 +0000

    feat!: rewrite

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,294 +1,103 @@
 import { assert, describe, it } from 'vitest'
-import { parse, pick, type ParseResult } from './index.js'
+import { pick } from './index.js'
 
-const assertResult = function (expected: Partial<ParseResult>, actual: Partial<ParseResult>): void {
-  assert.equal(actual.code, expected.code)
-
-  if (actual.script !== undefined || expected.script !== undefined) {
-    assert.equal(actual.script, expected.script)
-  }
-
-  if (actual.region !== undefined || expected.region !== undefined) {
-    assert.equal(actual.region, expected.region)
-  }
-
-  assert.equal(actual.quality, expected.quality)
-}
-
-describe('accept-language#parse()', function () {
+describe('accept-language', function () {
   it('empty', function () {
-    const result = parse('')
+    const result = pick('fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5', [])
 
-    assert.equal(result.length, 0)
-  })
-
-  it('should correctly parse the language with quality', function () {
-    const result = parse('en-GB;q=0.8')
-
-    assertResult({ code: 'en', quality: 0.8, region: 'GB' }, result[0])
-  })
-
-  it('should correctly parse the language without quality (default 1)', function () {
-    const result = parse('en-GB')
-    assertResult({ code: 'en', quality: 1, region: 'GB' }, result[0])
-  })
-
-  it('should correctly parse the language without region', function () {
-    const result = parse('en;q=0.8')
-    assertResult({ code: 'en', quality: 0.8 }, result[0])
-  })
-
-  // This needs to be changed to preserve the full code.
-  it('should ignore extra characters in the region code', function () {
-    const result = parse('az-AZ')
-    assertResult({ code: 'az', quality: 1, region: 'AZ' }, result[0])
-  })
-
-  it('should correctly parse a multi-language set', function () {
-    const result = parse('fr-CA,fr;q=0.8')
-    assertResult({ code: 'fr', quality: 1, region: 'CA' }, result[0])
-    assertResult({ code: 'fr', quality: 0.8 }, result[1])
-  })
-
-  it('should correctly parse a wildcard', function () {
-    const result = parse('fr-CA,*;q=0.8')
-    assertResult({ code: 'fr', quality: 1, region: 'CA' }, result[0])
-    assertResult({ code: '*', quality: 0.8 }, result[1])
-  })
-
-  it('should correctly parse a region with numbers', function () {
-    const result = parse('fr-150')
-    assertResult({ code: 'fr', quality: 1, region: '150' }, result[0])
-  })
-
-  it('should correctly parse complex set', function () {
-    const result = parse('fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1')
-    assertResult({ code: 'fr', quality: 1, region: 'CA' }, result[0])
-    assertResult({ code: 'fr', quality: 0.8 }, result[1])
-    assertResult({ code: 'en', quality: 0.6, region: 'US' }, result[2])
-    assertResult({ code: 'en', quality: 0.4 }, result[3])
-    assertResult({ code: '*', quality: 0.1 }, result[4])
-  })
-
-  it('should cope with random whitespace', function () {
-    const result = parse('fr-CA, fr;q=0.8,  en-US;q=0.6,en;q=0.4,    *;q=0.1')
-    assertResult({ code: 'fr', quality: 1, region: 'CA' }, result[0])
-    assertResult({ code: 'fr', quality: 0.8 }, result[1])
-    assertResult({ code: 'en', quality: 0.6, region: 'US' }, result[2])
-    assertResult({ code: 'en', quality: 0.4 }, result[3])
-    assertResult({ code: '*', quality: 0.1 }, result[4])
-  })
-
-  it('should sort based on quality value', function () {
-    const result = parse('fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5')
-    assertResult({ code: 'fr', quality: 1, region: 'CA' }, result[0])
-    assertResult({ code: 'en', quality: 0.6, region: 'US' }, result[1])
-    assertResult({ code: '*', quality: 0.5 }, result[2])
-    assertResult({ code: 'en', quality: 0.4 }, result[3])
-    assertResult({ code: 'fr', quality: 0.2 }, result[4])
-  })
-
-  it('should correctly identify script', function () {
-    const result = parse('zh-Hant-cn')
-    assertResult({ code: 'zh', quality: 1, region: 'cn', script: 'Hant' }, result[0])
-  })
-
-  it('should cope with script and a quality value', function () {
-    const result = parse('zh-Hant-cn;q=1, zh-cn;q=0.6, zh;q=0.4')
-    assertResult({ code: 'zh', quality: 1, region: 'cn', script: 'Hant' }, result[0])
-    assertResult({ code: 'zh', quality: 0.6, region: 'cn' }, result[1])
-    assertResult({ code: 'zh', quality: 0.4 }, result[2])
-  })
-})
-
-describe('accept-language#pick()', function () {
-  it('empty', function () {
-    const result = pick([], 'fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5')
-
-    assert.equal(result, undefined)
+    assert.deepEqual(result, [])
   })
 
   it('empty', function () {
-    const result = pick(
-      // @ts-expect-error test incorrect type
-      [true],
-      'fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5',
-    )
+    const result = pick('', [''])
 
-    assert.equal(result, undefined)
+    assert.deepEqual(result, [])
   })
 
   it('should pick a specific regional language', function () {
-    const result = pick(['en-US', 'fr-CA'], 'fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5')
-    assert.equal(result, 'fr-CA')
+    const result = pick('fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5', ['en-US', 'fr-CA'])
+    assert.deepEqual(result, ['fr-CA', 'en'])
   })
 
-  // it('should pick a specific regional language when accept-language is parsed', function () {
-  //   const result = pick(
-  //     ['en-US', 'fr-CA'],
-  //     parse('fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5')
-  //   )
-  //
-  //   assert.equal(result, 'fr-CA')
-  // })
+  it('should pick a specific regional language when accept-language is parsed', function () {
+    const result = pick('fr-CA,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5', ['en-US', 'fr-CA'])
+
+    assert.deepEqual(result, ['fr-CA', 'en'])
+  })
 
   it('should pick a specific script (if specified)', function () {
-    const result = pick(['zh-Hant-cn', 'zh-cn'], 'zh-Hant-cn,zh-cn;q=0.6,zh;q=0.4')
-    assert.equal(result, 'zh-Hant-cn')
+    const result = pick('zh-Hant-cn,zh-cn;q=0.6,zh;q=0.4', ['zh-Hant-cn', 'zh-cn'])
+    assert.deepEqual(result, ['zh-Hant-CN', 'zh'])
   })
 
   it('should pick proper language regardless of casing', function () {
-    const result = pick(['eN-Us', 'Fr-cA'], 'fR-Ca,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5')
+    const result = pick('fR-Ca,fr;q=0.2,en-US;q=0.6,en;q=0.4,*;q=0.5', ['eN-Us', 'Fr-cA'])
 
-    assert.equal(result, 'Fr-cA')
+    assert.deepEqual(result, ['fr-CA', 'en'])
   })
 
   it('should pick a specific language', function () {
-    const result = pick(['en', 'fr-CA'], 'ja-JP,ja;1=0.5,en;q=0.2')
-    assert.equal(result, 'en')
+    const result = pick('ja-JP,ja;1=0.5,en;q=0.2', ['en', 'fr-CA'])
+    assert.deepEqual(result, ['en'])
   })
 
   it('should pick a language when culture is not specified', function () {
-    const result = pick(['en-us', 'it-IT'], 'pl-PL,en')
-    assert.equal(result, 'en-us')
+    const result = pick('pl-PL,en', ['en-us', 'it-IT'])
+    assert.deepEqual(result, ['en'])
   })
 
   it('should return null if no matches are found', function () {
-    const result = pick(['ko-KR'], 'fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1')
-    assert.equal(result, null)
+    const result = pick('fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4', ['ko-KR'])
+    assert.deepEqual(result, [])
   })
 
   it('should return null if support no languages', function () {
-    const result = pick([], 'fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1')
-    assert.equal(result, null)
+    const result = pick('fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1', [])
+    assert.deepEqual(result, [])
   })
 
-  // it('should return null if invalid support', function () {
-  //   const result = pick(
-  //     undefined,
-  //     'fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1'
-  //   )
-  //   assert.equal(result, null)
-  // })
-
   it('should return null if invalid accept-language', function () {
-    const result = pick(['en'], '')
-    assert.equal(result, undefined)
+    const result = pick('', ['en'])
+    assert.deepEqual(result, [])
   })
 
   it('by default should be strict when selecting language', function () {
-    const result = pick(['en', 'pl'], 'en-US;q=0.6')
-    assert.equal(result, null)
+    const result = pick('en-US;q=0.6', ['en', 'pl'])
+    assert.deepEqual(result, ['en'])
   })
 
-  it('can select language loosely with an option', function () {
-    const result = pick(['en', 'pl'], 'en-US;q=0.6', { loose: true })
-    assert.equal(result, 'en')
+  it('selects the first matching language, even when supported language is more restrictive', function () {
+    const result = pick('en;q=0.6', ['en-US', 'en', 'pl'])
+    assert.deepEqual(result, ['en'])
   })
 
-  it('selects the first matching language in loose mode, even when supported language is more restrictive', function () {
-    const result = pick(['en-US', 'en', 'pl'], 'en;q=0.6', {
-      loose: true,
-    })
-    assert.equal(result, 'en-US')
+  it('selects the first matching language, even when the accepted language is more restrictive', function () {
+    const result = pick('en-US;q=0.6', ['en', 'en-US', 'pl'])
+    assert.deepEqual(result, ['en'])
   })
 
-  it('selects the first matching language in loose mode, even when the accepted language is more restrictive', function () {
-    const result = pick(['en', 'en-US', 'pl'], 'en-US;q=0.6', {
-      loose: true,
-    })
-    assert.equal(result, 'en')
+  it('quality is more important than order', function () {
+    const result = pick('fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1', ['en', 'fr'])
+    const result2 = pick('fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1', ['fr', 'en'])
+    assert.deepEqual(result, result2)
   })
 
-  it('quality is more important than order when matching loosely', function () {
-    const result = pick(['en', 'fr'], 'fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1', {
-      loose: true,
-    })
-    const result2 = pick(['fr', 'en'], 'fr-CA,fr;q=0.8,en-US;q=0.6,en;q=0.4,*;q=0.1', {
-      loose: true,
-    })
-    assert.equal(result, result2)
-  })
-
-  it('quality is more important than order when matching loosely2', function () {
-    const result = pick(['en', 'fr'], 'fr-CA,en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', {
-      loose: true,
-    })
-    const result2 = pick(['fr', 'en'], 'fr-CA,en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', {
-      loose: true,
-    })
-    assert.equal(result, result2)
+  it('quality is more important than order', function () {
+    const result = pick('fr-CA,en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', ['en', 'fr'])
+    const result2 = pick('fr-CA,en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', ['fr', 'en'])
+    assert.deepEqual(result, result2)
   })
 
   it('quality is more important than order when matching loosely3', function () {
-    const result = pick(['en', 'fr'], 'en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', {
-      loose: true,
-    })
-    const result2 = pick(['fr', 'en'], 'en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', { loose: true })
-    assert.equal(result, result2)
+    const result = pick('en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', ['en', 'fr'])
+    const result2 = pick('en-US;q=0.7,fr;q=0.6,en;q=0.4,*;q=0.1', ['fr', 'en'])
+    assert.deepEqual(result, result2)
   })
 
   it('should map zh-CHT => zh-Hant: Traditional Chinese', function () {
-    const result = pick(['zh-Hans', 'zh-Hant-CN', 'zh-Hant'], 'zh-CHT,zh-cn;q=0.6,zh;q=0.4')
-    assert.equal(result, 'zh-Hant')
-  })
-
-  it('should map zh-TW => zh-Hant-TW: Traditional Chinese / Taiwan', function () {
-    const result = pick(
-      ['zh-Hans-CN', 'zh-Hans-SG', 'zh-Hans', 'zh-Hant-TW'],
-      'zh-TW;q=0.6,zh;q=0.4',
-    )
-    assert.equal(result, 'zh-Hant-TW')
-  })
-
-  it('should map zh-HK => zh-Hant-HK: Traditional Chinese / Hong Kong', function () {
-    const result = pick(
-      ['zh-Hans-CN', 'zh-Hans-SG', 'zh-Hans', 'zh-Hant-HK', 'zh-Hant-TW'],
-      'zh-HK;q=0.6,zh;q=0.4',
-    )
-    assert.equal(result, 'zh-Hant-HK')
-  })
-
-  it('should map zh-MO => zh-Hant-MO: Traditional Chinese / Macau', function () {
-    const result = pick(
-      ['zh-Hans-CN', 'zh-Hans-SG', 'zh-Hans', 'zh-Hant-TW', 'zh-Hant-MO'],
-      'zh-MO;q=0.8,zh-TW;q=0.6,zh;q=0.4',
-    )
-    assert.equal(result, 'zh-Hant-MO')
-  })
-
-  it('should map zh-CHS to zh-Hans: Simplified Chinese', function () {
-    const result = pick(['zh-Hans-CN', 'zh-Hans', 'zh-Hans'], 'zh-CHS,zh-cn;q=0.6,zh;q=0.4')
-    assert.equal(result, 'zh-Hans')
-  })
-
-  it('should map zh-cn => zh-Hans-CN: Simplied Chinese / China', function () {
-    const result = pick(['zh-Hans-CN', 'zh-Hans-SG', 'zh-Hans', 'zh-Hant'], 'zh-cn;q=0.6,zh;q=0.4')
-    assert.equal(result, 'zh-Hans-CN')
-  })
-
-  it('should map zh-sg => zh-Hans-SG: Simplied Chinese / Singapore', function () {
-    const result = pick(['zh-Hans-CN', 'zh-Hans-SG', 'zh-Hans', 'zh-Hant'], 'zh-sg;q=0.6,zh;q=0.4')
-    assert.equal(result, 'zh-Hans-SG')
-  })
-})
-
-describe('readme examples', function () {
-  it('parse()', function () {
-    const result = parse('en-GB,en;q=0.8')
-    assert.deepEqual(result, [
-      { code: 'en', quality: 1, region: 'GB', script: undefined },
-      { code: 'en', quality: 0.8, region: undefined, script: undefined },
-    ])
-  })
-
-  it('pick() strict', function () {
-    const result = pick(['fr-CA', 'fr-FR', 'fr'], 'en-GB,en-US;q=0.9,fr-CA;q=0.7,en;q=0.8')
-    assert.equal(result, 'fr-CA')
-  })
-
-  it('pick() loose', function () {
-    const result = pick(['fr', 'en'], 'en-GB,en-US;q=0.9,fr-CA;q=0.7,en;q=0.8', { loose: true })
-    assert.equal(result, 'en')
+    const result = pick('zh-CHT,zh-cn;q=0.6,zh;q=0.4', ['zh-Hans', 'zh-Hant-CN', 'zh-Hant'], {
+      type: 'lookup',
+    })
+    assert.deepEqual(result, ['zh'])
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,132 +1,55 @@
 export type MapLocales = (value: string) => string
+import { basicFilter as basic, lookup } from 'bcp-47-match'
+import { bcp47Normalize } from 'bcp-47-normalize'
+import { parseAccept } from 'hono/utils/accept'
 
-export interface Options {
-  loose: boolean
-  mapLocales: MapLocales
+interface Options {
+  /**
+   * By default, when an error is encountered, an empty object is returned.
+   * When in forgiving mode, all found values up to the point of the error
+   * are included (`boolean`, default: `false`).
+   * So, for example, where by default `en-GB-abcdefghi` an empty object is
+   * returned (as the language variant is too long), in `forgiving` mode the
+   * `language` of `schema` is populated with `en` and the `region` is
+   * populated with `GB`.
+   *
+   * See https://github.com/wooorm/bcp-47
+   */
+  forgiving?: boolean
+  /**
+   *
+   * See https://github.com/wooorm/bcp-47-match
+   */
+  type?: 'basic' | 'lookup'
 }
 
-export interface ParseResult {
-  code: string
-  quality: number
-  region: string | undefined
-  script: string | undefined
-}
+const uniq = <T>(value: T[]) => [
+  ...new Set(value.filter((value) => typeof value === 'string' && value.length !== 0)),
+]
 
-// eslint-disable-next-line regexp/no-unused-capturing-group, regexp/no-misleading-capturing-group
-const regex = /((([A-Za-z]+(-[\dA-Za-z]+){0,2})|\*)(;q=[01](\.\d+)?)?)*/g
+const normalizeTags = (tags: string[], options: Options): string[] =>
+  uniq(
+    tags
+      .map((tag) => bcp47Normalize(tag, { forgiving: options.forgiving }))
+      .filter((value) => typeof value === 'string' && value.trim().length !== 0),
+  )
 
-function isString(s: unknown): s is string {
-  return typeof s === 'string'
-}
+const normalizeRanges = (acceptLanguage: string, options: Options): string[] =>
+  uniq(
+    parseAccept(acceptLanguage)
+      .map((value) => value.type)
+      .map((tag) => (tag === '*' ? '*' : bcp47Normalize(tag, { forgiving: options.forgiving }))),
+  )
 
-export function parse(al: string, mapLocale: MapLocales = (x) => x): ParseResult[] {
-  const strings = (al.length > 0 ? al : '').match(regex)
+export function pick(acceptLanguage: string, tags: string[], options: Options = {}): string[] {
+  const match = (
+    {
+      basic,
+      lookup,
+    } as const
+  )[options.type ?? 'basic']
 
-  if (strings === null) {
-    return []
-  }
+  const value = match(normalizeTags(tags, options), normalizeRanges(acceptLanguage, options))
 
-  return (
-    strings
-      .map((m) => {
-        if (m.length === 0) {
-          return
-        }
-
-        const bits = m.split(';')
-        const ietf = mapLocale(bits[0]).split('-')
-        const hasScript = ietf.length === 3
-
-        const script = hasScript ? ietf[1] : undefined
-        const region = hasScript ? ietf[2] : ietf[1]
-
-        return {
-          code: ietf[0],
-          quality:
-            isString(bits[1]) && bits[1].length !== 0 ? parseFloat(bits[1].split('=')[1]) : 1,
-          region,
-          script,
-        }
-      })
-      .filter((r) => r !== undefined) as ParseResult[]
-  ).sort((a, b) => b.quality - a.quality)
-}
-
-// zh-CN = Simplified script with Mandarin grammar = Chinese as written in China
-// zh-TW = Traditional script with Mandarin grammar = Chinese as written in Taiwan
-// zh-HK = Traditional script with Cantonese grammar = Chinese as written in Hong Kong
-
-const newLocales: Record<string, string> = {
-  'zh-chs': 'zh-Hans',
-  // https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/dd997383(v=vs.100)
-  'zh-cht': 'zh-Hant',
-
-  'zh-cn': 'zh-Hans-CN',
-  'zh-hk': 'zh-Hant-HK',
-  'zh-mo': 'zh-Hant-MO',
-
-  // https://gist.github.com/amake/0ac7724681ac1c178c6f95a5b09f03ce
-  'zh-sg': 'zh-Hans-SG',
-  'zh-tw': 'zh-Hant-TW',
-}
-
-function mapLocales(locale: string): string {
-  let value = locale.toLowerCase().replace(/_/g, '-')
-  value = newLocales[value] ?? value
-
-  return value
-}
-
-export function pick<T extends string>(
-  languages: T[],
-  acceptLanguage: string,
-  options: Partial<Options> = {},
-): T | undefined {
-  const langs = languages.filter((string) => isString(string))
-
-  if (langs.length === 0 || !isString(acceptLanguage)) {
-    return undefined
-  }
-
-  const _options = { loose: false, mapLocales, ...options }
-
-  const parsed = parse(acceptLanguage, _options.mapLocales)
-
-  const supported = langs.map((support) => {
-    const bits = support.split('-')
-    const hasScript = bits.length === 3
-
-    const script = hasScript ? bits[1] : undefined
-    const region = hasScript ? bits[2] : bits[1]
-
-    return {
-      code: bits[0],
-      region,
-      script,
-    }
-  })
-
-  // eslint-disable-next-line typescript/prefer-for-of
-  for (let index = 0; index < parsed.length; index++) {
-    const lang = parsed[index]
-    const langCode = lang.code?.toLowerCase()
-    const langRegion = lang.region?.toLowerCase()
-    const langScript = lang.script?.toLowerCase()
-
-    for (let index = 0; index < supported.length; index++) {
-      const value = supported[index]
-
-      if (
-        langCode === value.code?.toLowerCase() &&
-        (_options.loose ||
-          langScript === undefined ||
-          langScript === value.script?.toLowerCase()) &&
-        (_options.loose || langRegion === undefined || langRegion === value.region?.toLowerCase())
-      ) {
-        return languages[index]
-      }
-    }
-  }
-
-  return undefined
+  return typeof value === 'string' ? [value] : (value ?? [])
 }
